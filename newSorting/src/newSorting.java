11ce14d7000dfe8341a937dce16d017adf04fab6
Luis Aguirre <luis.alejandro.aguirre@outlook.com>
2020-04-06 21:39:44 -0600

    Added comments to newSorting.java and finished newSortingTester.java.

--- newSorting/src/newSorting.java
+++ newSorting/src/newSorting.java
@@ -1,29 +1,36 @@
 public class newSorting {
 
-    public static void newSorting(int[] A, int size){
+    public void newSorting(int[] A, int size){
+
+        // Base case
         if(A.length <= size){
             quickSort(A, 0, A.length - 1);
         } else {
-            int[] left = new int[A.length / 2];
-            int[] right = new int[A.length - left.length];
+            int[] left = new int[A.length / 2];                     // New array created from left side of array to middle index
+            int[] right = new int[A.length - left.length];          // New array created from middle + 1 to end of array
 
+            // Copy elements from original array to left
             for(int i = 0; i < left.length; i++){
                 left[i] = A[i];
             }
+
+            // Copy elements from original array to right
             for(int i = 0; i < right.length; i++){
                 right[i] = A[left.length + i];
             }
-            newSorting(left, size);
-            newSorting(right, size);
-            mergeSortedHalves(A, left, right);
+
+            newSorting(left, size);                     // Recursive call for left side of A array
+            newSorting(right, size);                    // Recursive call for right side of A array
+            mergeSortedHalves(A, left, right);          // Merge sorted halves
         }
     }
 
-    public static void quickSort(int[] A, int low, int high){
-        int pivot = A[low];
-        int i = low;
-        int j = high;
+    public void quickSort(int[] A, int low, int high){
+        int pivot = A[low];         // Pivot will always be index 0
+        int i = low;                // Counter for finding elements lower than pivot
+        int j = high;               // Counter for finding elements higher than pivot
 
+        // Swap elements based on index
         while(i < j){
             while(A[i] <= pivot && i < j){
                 i++;
@@ -38,21 +45,25 @@ public class newSorting {
             }
         }
 
+        // Swap pivot to middle of array
         A[low] = A[j];
         A[j] = pivot;
 
+        // Recursive call for elements less than pivot
         if(low < j - 1){
             quickSort(A, low, j - 1);
         }
 
+        // Recursive call for elements greater than pivot
         if(j + 1 < high){
             quickSort(A, j + 1, high);
         }
     }
 
-    public static void mergeSortedHalves(int[] A, int[] left, int[] right){
+    public void mergeSortedHalves(int[] A, int[] left, int[] right){
         int i = 0, j = 0, k = 0;
 
+        // Merge elements in order
         while(i < left.length && j < right.length){
             if(left[i] <= right[j]){
                 A[k++] = left[i++];
@@ -61,10 +72,12 @@ public class newSorting {
             }
         }
 
+        // Copy remaining elements of left if there are any left
         while(i < left.length){
             A[k++] = left[i++];
         }
 
+        // Copy remaining elements of right if there are any left
         while(j < right.length){
             A[k++] = right[j++];
         }
--- newSorting/src/newSortingTester.java
+++ newSorting/src/newSortingTester.java
@@ -6,41 +6,84 @@ public class newSortingTester {
 
     @Test
     /*
-
+    Case will test basic functionality of newSorting() method. A unsorted array of 8 elements will be created. Method
+    should be able to arrange the elements of the array in increasing order. Size at which method will perform
+    quickSort is 3 so the array is divided unevenly and we can test if dividing the array and later merging it is
+    working as expected.
      */
-    public void createFromRNASequenceTest1() {
+    public void newSortingTest1() {
+        int[] unsorted = {7, 98, 23, 11, 0, 15, 42};
+        int[] sorted = {0, 7, 11, 15, 23, 42, 98};
 
+        newSorting test = new newSorting();
+        test.newSorting(unsorted, 3);
+
+        assertArrayEquals(sorted, unsorted);
     }
 
     @Test
     /*
-
+    Case will test how method handles negative number. An unsorted array will be created with 10 randomly selected elements
+    some of which will be negative numbers. Negative elements should be sorted at the start of array.
+    Size at which method will perform quickSort is 4.
      */
-    public void createFromRNASequenceTest2() {
+    public void newSortingTest2() {
+        int[] unsorted = {204, 5, 76, -34, 122, 83, -6, -84, 74, -1};
+        int[] sorted = {-84, -34, -6, -1, 5, 74, 76, 83, 122, 204};
 
+        newSorting test = new newSorting();
+        test.newSorting(unsorted, 4);
+
+        assertArrayEquals(sorted, unsorted);
     }
 
     @Test
     /*
-
+    Case will test how method deals with large arrays. An unsorted array of 30 elements will be created using randomly
+    selected elements. Method should work the same as with previous test cases and should return a sorted array regardless
+    of length but will be tested to see if big arrays break something in the code.
+    Size at which method will perform quickSort is 7.
      */
-    public void createFromRNASequenceTest3() {
+    public void newSortingTest3() {
+        int[] unsorted = {305, -3, 0, 85, 39, 90, 104, 278, 56, -11, -76, 8, 189, -96, 3, 41, -7, 504, 67, -1, 25, 208, -691, 82, 711, 84, 0, 1, 7, -3, 100};
+        int[] sorted = {-691, -96, -76, -11, -7, -3, -3, -1, 0, 0, 1, 3, 7, 8, 25, 39, 41, 56, 67, 82, 84, 85, 90, 100, 104, 189, 208, 278, 305, 504, 711};
 
+        newSorting test = new newSorting();
+        test.newSorting(unsorted, 7);
+
+        assertArrayEquals(sorted, unsorted);
     }
 
     @Test
     /*
-
+    Case will test how the method handles arrays that go straight to the quickSort() method without passing through
+    the recursive newSorting() methods. An unsorted array of size 5 will be created with random elements in it.
+    Size at which method will perform quickSort is 6 so code goes directly into the base case.
      */
-    public void createFromRNASequenceTest4() {
+    public void newSortingTest4() {
+        int[] unsorted = {76, 104, 0, 2, 24};
+        int[] sorted = {0, 2, 24, 76, 104};
 
+        newSorting test = new newSorting();
+        test.newSorting(unsorted, 6);
+
+        assertArrayEquals(sorted, unsorted);
     }
 
     @Test
     /*
-
+    Case will test newSorting method with an already sorted array with duplicate elements in it. A sorted array of size 11
+    will be created with some elements in it being duplicates. Method should iterate through the array but no elements should
+    change position since it is already sorted. Duplicate elements should also remain in place.
+    Size at which method will perform quickSort is 5.
      */
-    public void createFromRNASequenceTest5() {
+    public void newSortingTest5() {
+        int[] unsorted = {-4, -4, 0, 23, 35, 59, 59, 63, 97, 150, 205};
+        int[] sorted = {-4, -4, 0, 23, 35, 59, 59, 63, 97, 150, 205};
 
+        newSorting test = new newSorting();
+        test.newSorting(unsorted, 5);
+
+        assertArrayEquals(sorted, unsorted);
     }
 }
